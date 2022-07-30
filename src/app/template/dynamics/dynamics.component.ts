333f1e475c4a810a5ccff7d474cb6eaca18d3662
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-07-30 09:44:20 -0500

    Dynamic forms done

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,20 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Persona {
+  nombre: string
+  favoritos: Favorito[]
+}
+
+interface Favorito {
+  id: number
+  nombre: string
+}
+
 @Component({
   selector: 'app-dynamics',
   templateUrl: './dynamics.component.html',
   styles: [
   ]
 })
-export class DynamicsComponent implements OnInit {
+export class DynamicsComponent {
 
-  constructor() { }
+  nuevoJuego: string = '';
 
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Leiner',
+    favoritos: [
+      { id: 1, nombre: 'Iron Man' },
+      { id: 2, nombre: 'Capitán América' }
+    ]
   }
 
   save() {
     console.log('Formulario posteado');
+  }
+
+  agregar() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
   }
 
 }
