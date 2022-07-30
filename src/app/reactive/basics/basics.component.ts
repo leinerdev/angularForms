8f809a445c3789d5df91730d4c253ddb12e041de
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-07-30 12:45:18 -0500

    Basic reactive forms

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicsComponent implements OnInit {
 
-  constructor() { }
+  // myForm: FormGroup = new FormGroup({
+  //   nombre      : new FormControl('RTX 4080Ti'),
+  //   precio      : new FormControl(1500),
+  //   existencias : new FormControl(5),
+  // });
+
+  myForm: FormGroup = this.fb.group({
+    nombre      : [, [Validators.required, Validators.minLength(3)]],
+    precio      : [, [Validators.required, Validators.min(0)]],
+    existencias : [, [Validators.required, Validators.min(0)]],
+  });
+
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
+      this.myForm.reset({
+        nombre: 'RTX 4080Ti',
+      });
+  }
+
+  campoEsValido(campo: string) {
+    return this.myForm.controls[campo].touched && this.myForm.controls[campo].errors;
+  }
+
+  guardar() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();
   }
 
 }
