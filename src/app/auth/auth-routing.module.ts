4e40a725024056eb3e7d38beb9e94afb5aeee36e
leiner.barrios@inglesparatodos.edu.co <devappipt@Devs-iMac.local>
2022-07-30 17:18:32 -0500

    Validate name and last name with validators pattern

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
@@ -7,8 +8,8 @@ const routes: Routes = [
   {
     path: '',
     children: [
-      { path: 'register', component: RegisterComponent },
       { path: 'login', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
       { path: '**', redirectTo: 'register' }
     ]
   }
--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styles: [``]
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+
+  myForm: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]]
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  campoNoValido(campo: string) {
+    return this.myForm.get(campo)?.invalid && this.myForm.get(campo)?.touched;
+  }
+
+  submitForm() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
   }
 
 }
